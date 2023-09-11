9a959597d3a89b7b8ec24a5071c8c3e59cb7587e
ImaneBirJmel <93131576+ImaneBirJmel@users.noreply.github.com>
2023-09-11 18:58:41 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+
+<div align="center">
+
+# YOLOv8 Streamlit APP trained by customer dataset
+
+  <p>
+    <a align="center" href="https://ultralytics.com/yolov8" target="_blank">
+     
+  </p>
+
+
+</div>
+
+
+## Features
+- Feature1: Object detection task.
+- Feature2: YOLOv8 model trained by high voltage cables images (customer dataset)
+- Feature3: Multiple input formats. `Image`, `Webcam`
+
+## Interactive Interface
+### Image Input Interface
+![image_input_demo](https://github.com/ImaneBirJmel/accessories_detection/blob/main/test.jpg)
+
+### Webcam Input Interface
+![webcam_input_demo](https://github.com/ImaneBirJmel/accessories_detection/blob/main/test.jpg)
+
+
+## Installation
+
+### Install packages
+```commandline
+# yolov8 dependencies
+pip install ultralytics
+
+# Streamlit dependencies
+pip install streamlit
+```
+
+
+## Run
+```commandline
+streamlit run app.py
+```
+Then will start the Streamlit server and open your web browser to the default Streamlit page automatically.
+
+
+***
+
+
---
+++ app.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from PIL import Image
+import streamlit as st
+
+import config
+from utils import load_model, infer_uploaded_image, infer_uploaded_webcam, seq_detection
+
+import cv2
+# setting page layout
+st.set_page_config(
+    page_title="Detection of HV Cable Accessories Sequencing Using DL Model",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+    )
+
+# main page heading
+st.title("Detection of High-Voltage Cable Accessories Sequencing Using DL Model")
+
+# sidebar
+st.sidebar.header("DL Model Config")
+
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 30, 100, 50)) / 100
+
+model_path = "best.pt"
+
+# load pretrained DL model
+try:
+    model = load_model(model_path)
+except Exception as e:
+    st.error(f"Unable to load model. Please check the specified path: {model_path}")
+
+# 
+source_selectbox = st.sidebar.selectbox(
+    "Select Source",
+    config.SOURCES_LIST
+)
+###
+
+
+###
+source_img = None
+if source_selectbox == config.SOURCES_LIST[0]: # Image
+    labels, image_path=infer_uploaded_image(confidence, model)  # Obtenez les labels et le chemin de l'image 
+    seq_detection(labels, image_path) # Appelez la fonction seq_detection avec les labels et le chemin de l'image pour detecter le séquencement des objets
+
+elif source_selectbox == config.SOURCES_LIST[1]: # Webcam
+    infer_uploaded_webcam(confidence, model)
+    seq_detection(labels, image_path)
+else:
+    st.error("Currently only 'Image' and 'Video' source are implemented")
