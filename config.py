20361713b582415abecfd0a2b781919bf6e015e8
ImaneBirJmel <93131576+ImaneBirJmel@users.noreply.github.com>
2023-09-18 20:39:50 +0100

    config.py

--- config.py
+++ config.py
@@ -1,21 +1,13 @@
-from pathlib import Path
+from pathlib import Path 
 import sys
-
-# Get the absolute path of the current file
-file_path = Path(__file__).resolve()
-
-# Get the parent directory of the current file
-root_path = file_path.parent
-
-# Add the root path to the sys.path list if it is not already there
-if root_path not in sys.path:
-    sys.path.append(str(root_path))
-
-# Get the relative path of the root directory with respect to the current working directory
-ROOT = root_path.relative_to(Path.cwd())
-
-
-# Source
-SOURCES_LIST = ["Image", "Webcam"]
-
-
+# Obtient le chemin absolu du fichier courant
+chemin_fichier = Path(__file__).resolve()
+# Obtient le répertoire parent du fichier courant
+chemin_racine = chemin_fichier.parent
+# Ajoute le chemin racine à la liste sys.path s'il n'est pas déjà présent
+if chemin_racine not in sys.path:
+    sys.path.append(str(chemin_racine))
+# Obtient le chemin relatif du répertoire racine par rapport au répertoire de travail courant
+RACINE = chemin_racine.relative_to(Path.cwd())
+# Liste des sources
+LISTE_SOURCES = ["Image", "Webcam"]
